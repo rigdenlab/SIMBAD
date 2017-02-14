b6b07c9ccd4b3c65220b0678b76dfd3226cfd343
hlasimpk <hlasimpk@liv.ac.uk>
2017-02-14 15:55:07 +0100

    added location of the contaminant models to the constants file

--- simbad/constants.py
+++ simbad/constants.py
@@ -14,3 +14,6 @@ SIMBAD_CONFIG_FILE = os.path.join(SIMBAD_EGG_ROOT, 'static', 'simbad.ini')
 
 # SIMBAD database for lattice search
 SIMBAD_LATTICE_DB = os.path.join(SIMBAD_EGG_ROOT, 'static', 'niggli_database.cpk')
+
+# SIMBAD database of contaminant models
+CONTAMINANT_MODELS = os.path.join(SIMBAD_EGG_ROOT, 'static', 'contaminant_models')
