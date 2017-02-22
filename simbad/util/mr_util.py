27a1f62b20f3735828ae84e55d7da1dfd51e21cb
hlasimpk <hlasimpk@liv.ac.uk>
2017-02-22 13:28:28 +0100

    Added in MR/Refinement using MrBump code and multiprocessing for local use

--- simbad/util/mr_util.py
+++ simbad/util/mr_util.py
@@ -6,6 +6,14 @@ Class to run MR on SIMBAD results using code from MrBump
 
 import os
 import sys
+import logging
+import types
+import copy_reg
+from contextlib import contextmanager
+import multiprocessing
+
+from simbad.util import simbad_util
+from simbad.constants import SIMBAD_EGG_ROOT
 
 # Set up MrBUMP imports
 if os.path.isdir(os.path.join(os.environ["CCP4"], "share", "mrbump")):
@@ -30,57 +38,70 @@ sys.path.append(os.path.join(mrbump_incl, 'phasing'))
 
 import cluster_run
 
-import MRBUMP_initialise
-import MRBUMP_master
-
-import MRBUMP_Molrep
-import MRBUMP_Phaser
-
-"""Currently taking reading through Ronan's MR script in MrBump and determining the input variables needed by MRBUMP_Molrep/Phaser"""
-
-class MR_submit(object):
-    '''Class to submit MR job'''
-
-    def __init__(self, optd):
-        """NEED TO DECIDE WHAT TO DO HERE"""
-
-        return
-
-    def MR_is_done(self, init, mstat):
-        """ A function that returns True if there are no more jobs to submit"""
-
-        # NEED TO GIVE A SORTED MODEL LIST
-        for model in mstat.sorted_model_list:
-
-            # NEED TO GIVE MR PROGRAM TO USE
-            for MRPROGRAM in init.keywords.MR_PROGRAM_LIST:
-
-                # Set the MR program : NEED TO FIND OUT WHAT MSTAT IS HERE
-                mstat.setMRPROGRAM(MRPROGRAM.upper())
-
-                # DICTIONARY? Containing model path?
-                MODEL = mstat.model_list[model]
-
-                if mstat.MRPROGRAM == "MOLREP" and not MODEL.isMolrepSubmitted() or \
-                   mstat.MRPROGRAM == "PHASER" and not MODEL.isPhaserSubmitted():
-                    return False
-            return True
+_logger = logging.getLogger(__name__)
 
 
 
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+copy_reg.pickle(types.MethodType, _pickle_method)
 
 class MR_cluster_submit(object):
     '''Class to run MR on a cluster'''
 
     def __init__(self, optd):
         self.input_file = None
-        self.mr_keyfile = None
         self.mr_program = None
+        self.mr_keyfile = None
+        self.refine_program = None
         self.refine_keyfile = None
-        self.refine_program = "refmac5"
 
-        self.parse_options(optd)
+        self.optd = optd
 
+    def parse_options(self, model):
+        '''Function to set up input options for the job'''
+
+        self.input_file = os.path.join(self.optd.d['work_dir'], 'MR_LATTICE', model, 'input.txt')
+
+        # Set path to MR keyword file
+        self.mr_program = self.optd.d['MR_program']
+        if self.mr_program.upper() == 'MOLREP' and not self.optd.d['mr_keywords']:
+            self.mr_keyfile = os.path.join(SIMBAD_EGG_ROOT, 'static', 'default_molrep_keywords.txt')
+        elif self.mr_program.upper() == 'PHASER' and not self.optd.d['mr_keywords']:
+            self.mr_keyfile = os.path.join(SIMBAD_EGG_ROOT, 'static', 'default_phaser_keywords.txt')
+        elif self.optd.d['mr_keywords']:
+            self.mr_keyfile = self.optd.d['mr_keywords']
+        else:
+            msg = "Unable to find MR keyword file for MR program: {0}".format(optd.d['MR_program'])
+            _logger.debug(msg)
+            raise RuntimeError(msg)
+
+        # Set path to refinement keyword file
+        self.refine_program = self.optd.d['refine_program']
+        if self.refine_program.upper() == "REFMAC5" and not self.optd.d['refine_keywords']:
+            self.refine_keyfile = os.path.join(SIMBAD_EGG_ROOT, 'static', 'default_refmac_keywords.txt')
+        elif self.optd.d['refine_keywords']:
+            self.refine_keyfile = self.optd.d['refine_keywords']
+        else:
+            msg = "Unable to find refinement keyword file for refinement program: {0}".format(self.optd.d['refine_program'])
+            _logger.debug(msg)
+            raise RuntimeError(msg)
+        return
+
+    def run_job(self, model):
+        _logger.info("Running MR and refinement on {0}".format(model))
+
+        # parse options for the model
+        self.parse_options(model)
+
+        # Generate MR input files
+        self.MR_setup(model)
+
+        # Run job
         cljob = cluster_run.ClusterJob()
 
         if self.input_file:
@@ -88,86 +109,106 @@ class MR_cluster_submit(object):
             cljob.parse_input(self.input_file)
 
         if self.mr_keyfile and self.mr_program and self.refine_keyfile and self.refine_program:
-            # Run cluster job
-            cljob.run(self.mr_program, self.refine_program, self.mr_keyfile, refine_keyfile=self.refine_keyfile)
+            with suppress_stdout():
+                cljob.run(self.mr_program, self.refine_program, self.mr_keyfile, refine_keyfile=self.refine_keyfile)
 
-    def parse_options(self, optd):
-        '''Function to set up input files for the MR job'''
+        if self.optd.d['early_term']:
+            terminate = self.solution_found(model)
+            return terminate
 
-        # NEED TO FIND OUT ALL THE OPTIONS THAT NEED TO BE INPUT HERE AND ADD LOGIC TO DECIDE WHICH FILES TO INPUT BASED ON TYPE OF RUN
+        return False
 
-        self.input_file = None
+    def MR_setup(self, model):
+        """Code to generate directories for MR and refinement for a model
+        and generate an input file containing information about the MR/refine run"""
 
-        # input file needs to contain a list of input settings including:
-        # DIRE = directory
-        # SGIN = Space group in
-        # HKL1 = HKL 1 input file
-        # HKLR = HKL refined input file (file name HKL1 gets copied to)
+        self.optd.d['model'] = model
+        # Create individual directories for every results
+        if self.optd.d['MR_program'].upper() == "MOLREP":
+            os.chdir(self.optd.d['work_dir'])
+            os.mkdir(os.path.join(self.optd.d['mode'], model))
+            os.mkdir(os.path.join(self.optd.d['mode'], model, 'mr'))
+            os.mkdir(os.path.join(self.optd.d['mode'], model, 'mr', 'molrep'))
+            os.mkdir(os.path.join(self.optd.d['mode'], model, 'mr', 'molrep', 'refine'))
+        elif self.optd.d['MR_program'].upper() == "PHASER":
+            os.chdir(self.optd.d['work_dir'])
+            os.mkdir(os.path.join(self.optd.d['mode'], model))
+            os.mkdir(os.path.join(self.optd.d['mode'], model, 'mr'))
+            os.mkdir(os.path.join(self.optd.d['mode'], model, 'mr', 'phaser'))
+            os.mkdir(os.path.join(self.optd.d['mode'], model, 'mr', 'phaser', 'refine'))
 
-        # If MR program == phaser
-        # HKLO = phaser HKL output file
-
-        # PDBO = MR PDB output
-        # MRLO = MR log
-
-        # REFH = refinement HKL out
-        # REFP = refinement PDB out
-
-        # ENAN = try enantiomorph forms < True | False >
-
-        # FPIN = FP flag
-        # SIGF = SIGF flag
-        # FREE = Free flag
-        # SOLV = solvent content
-        # RESO = resolution
-
-        # PDBI = PDB input
-
-
-        self.mr_program = optd.d['MR_program']
-        self.refine_program = optd.d['refine_program']
-
-        # Need to find out what the mr/refine key files are and add them in here
-        self.mr_keyfile = ""
-        # Just a file containing MOLREP key words - full path needs to be given, can be empty for now
-
-        self.refine_keyfile = ""
-        # Just a file containing PHASER key words - full path needs to be given, can be empty for now
+        if self.optd.d['mode'] == 'MR_LATTICE':
+            simbad_util.generate_mr_input_file(self.optd, model, 'lattice')
 
         return
 
+    def multiprocessing(self, results):
+        """Code to run MR and refinement in parallel"""
+
+        def run(job_queue):
+            """processes element of job queue if queue not empty"""
+            TIME_OUT_IN_SECONDS = 60
+
+            while not job_queue.empty():
+                name = job_queue.get(timeout=TIME_OUT_IN_SECONDS)
+                terminate = self.run_job(name)
+
+                if terminate:
+                    print "MR with {0} was successful so removing remaining jobs from inqueue".format(name)
+                    while not job_queue.empty():
+                        job = job_queue.get()
+                        _logger.debug("Removed job [{0}] from inqueue".format(job))
 
 
 
-# POTENTIAL CODE TO ADD TO __INIT__
+        # Create job queue
+        job_queue = multiprocessing.Queue()
 
-# if type == "lattice":
-#     self.results_file = os.path.join(optd.d["work_dir"], "lattice.csv")
-#     self.structure_dir = os.path.join(optd.d["work_dir"], "lattice_input_models")
-#     self.ncyc = optd.d["ncyc_lattice"]
-#
-# elif type == "contaminant":
-#     self.results_file = os.path.join(optd.d["work_dir"], "contaminant.csv")
-#
-#     # Check to see if user defined contaminant database present, if not use the default
-#     if optd.d["Contaminant_database"]:
-#         self.structure_dir = optd.d["Contaminant_database"]
-#     else:
-#         self.structure_dir = CONTAMINANT_MODELS
-#     self.ncyc = optd.d["ncyc_contam"]
-#
-# elif type == "full":
-#     self.results_file = os.path.join(optd.d["work_dir"], "full.csv")
-#
-#     if optd.d["MoRDa_database"]:
-#         self.structure_dir = optd.d["MoRDa_database"]
-#         self.ncyc = optd.d["ncyc_full"]
-#     else:
-#         msg = "Path to MoRDa database not specified"
-#         raise RuntimeError(msg)
-#         exit_util.exit_error(msg)
-# else:
-#     msg = "Type of MOLREP run not specified"
-#     raise RuntimeError(msg)
-#     exit_util.exit_error(msg)
+        # Add each result from results to the job queue
+        for result in results:
+            job_queue.put(result)
+
+
+        processes = []
+        # Set up processes equal to the number of processors input
+        for i in range(self.optd.d['nproc']):
+            process = multiprocessing.Process(target=run, args=(job_queue,))
+            process.start()
+            processes.append(process)
+
+        # block the calling thread
+        for p in processes:
+            p.join()
+
+
+    def solution_found(self, model):
+        """Function to check is a solution has been found"""
+        # Set default values if no results found
+        final_r_fact = 1
+        final_r_free = 1
+
+        with open(os.path.join(self.optd.d['work_dir'], self.optd.d['mode'], model,
+                               'mr', 'molrep', 'refine', model + '_ref.log'), 'r') as f:
+            for line in f:
+                if line.startswith('           R factor'):
+                    final_r_fact = float(line.split()[2])
+                elif line.startswith('             R free'):
+                    final_r_free = float(line.split()[2])
+
+        if final_r_fact < 0.45 and final_r_free < 0.45:
+            return True
+        else:
+            return False
+
+
+@contextmanager
+def suppress_stdout():
+    """Code to suppress the stdout of a function, used here to silence stdout of MrBUMP modules"""
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
 
