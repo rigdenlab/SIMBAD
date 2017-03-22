7b091f64afb42d91e56cb2bd95f13576cfad78bc
hlasimpk <hlasimpk@liv.ac.uk>
2017-03-22 15:07:55 +0100

    Added in arguments for early termination and enantiomorphs

--- simbad/command_line/simbad_lattice.py
+++ simbad/command_line/simbad_lattice.py
@@ -20,6 +20,10 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument('-d', '--database', default=simbad.constants.SIMBAD_LATTICE_DB,
                    help="The path to the LATTICE database")
+    p.add_argument('-early_term', default=True,
+                   help="Terminate the program early if a solution is found <True | False>")
+    p.add_argument('-enam', default=False,
+                   help="Trial enantimorphic space groups <True | False>")
     p.add_argument('-mr_program', default='molrep',
                    help="The name of the molecular replacement program to use <molrep|phaser>")
     p.add_argument('-refine_program', default='refmac5',
@@ -44,7 +48,7 @@ def main():
     lattice_search.download_results(args.working_dir)
     model_dir = os.path.join(args.working_dir, 'lattice_input_models')
     molecular_replacement = simbad.util.mr_util.MrSubmit(args.mtz, args.mr_program, args.refine_program, model_dir,
-                                                         args.output_dir)
+                                                         args.output_dir, args.early_term, args.enam)
     molecular_replacement.multiprocessing(lattice_search.search_results, nproc=args.nproc)
     molecular_replacement.summarize()
 
