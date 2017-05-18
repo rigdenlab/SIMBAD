1aaed8d688e13858d4d58ee091bb26e08354a7d2
hlasimpk <hlasimpk@liv.ac.uk>
2017-05-18 14:25:06 +0200

    Added example data/scripts for various SIMBAD runs

---
+++ simbad/examples/contaminant_example/contamiant_example_script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env ccp4-python
+
+"""Example script to run SIMBAD contaminant search on 1TXX test case"""
+
+simbad-contaminant 1txx.mtz
---
+++ simbad/examples/lattice_example/lattice_example_script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env ccp4-python
+
+"""Script to run SIMBAD lattice search on TOXD test case"""
+
+simbad-lattice toxd.mtz
