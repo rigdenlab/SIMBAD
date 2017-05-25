37e752883bfc2b12c3adb34a4aeb4c49ff31034b
Felix Simkovic <felixsimkovic@me.com>
2017-05-25 16:00:27 +0100

    respect early_term bool

--- simbad/mr/__init__.py
+++ simbad/mr/__init__.py
@@ -400,7 +400,10 @@ class MrSubmit(object):
         # Execute the scripts
         j = mbkit.dispatch.Job(submit_qtype)
         j.submit(scripts, nproc=nproc, job_name='simbad_mr', submit_queue=submit_queue)
-        j.wait(monitor=monitor, check_success=mr_succeeded_log)
+        if self.early_term:
+            j.wait(monitor=monitor, check_success=mr_succeeded_log)
+        else:
+            j.wait(monitor=monitor)
 
         for result in results:
             # Set default values
