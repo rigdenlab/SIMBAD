b1bdc9ff658ad9ca645dcafab6edc7f8b9527091
Felix Simkovic <felixsimkovic@me.com>
2017-05-27 08:23:41 +0100

    removed redundant code

--- simbad/util/simbad_util.py
+++ simbad/util/simbad_util.py
@@ -7,19 +7,12 @@ __version__ = "1.0"
 import logging
 import os
 import string
-import subprocess
-import sys
-import tempfile
 
 import iotbx.pdb
 import iotbx.pdb.amino_acid_codes
 
 import mbkit.chemistry
 import mbkit.dispatch.cexectools
-
-SCRIPT_EXT = '.bat' if sys.platform.startswith('win') else '.sh'
-EXE_EXT = '.exe' if sys.platform.startswith('win') else ''
-SCRIPT_HEADER = '' if sys.platform.startswith('win') else '#!/bin/bash'
 
 logger = logging.getLogger(__name__)
 
@@ -32,62 +25,6 @@ def filename_append(filename=None, astr=None, directory=None, separator="_"):
     if directory is None:
         directory = dirname
     return os.path.join(directory, name)
-
-
-def run_job(cmd, logfile=None, directory=None, stdin=None):
-    """Execute a command and return the exit code.
-
-    Parameters
-    ----------
-    cmd : list
-       Command to run as a list
-    stdin : str, optional
-       Stdin for the command
-    logfile : str, optional
-       The path to the logfile
-    directory : str, optional
-       The directory to run the job in (cwd assumed)
-    stdin : str
-       Additional keys to go into STDIN 
-
-    Returns
-    -------
-    returncode : int
-       Subprocess exit code
-
-    Notes
-    -----
-    We take care of outputting stuff to the logs and opening/closing logfiles
-
-    """
-    if not directory:
-        directory = os.getcwd()
-    
-    logger.debug("Running job in %s\n%s\n%s", directory, " ".join(cmd), stdin)
-
-    kwargs = {"bufsize":0, "shell":"False"} if os.name == "nt" else {}
-    p = subprocess.Popen(
-        cmd, stdin=subprocess.PIPE, cwd=directory, stdout=subprocess.PIPE, 
-        stderr=subprocess.STDOUT, **kwargs
-    )
-
-    # Write the keyword input
-    if stdin is not None:
-        p.stdin.write(stdin)
-        p.stdin.close()
-
-    # Watch the output for successful termination
-    if logfile is None:
-        logfile = os.devnull
-
-    with open(logfile, "w") as f_out:
-        for line in p.stdout:
-            f_out.write(line)
-
-    p.wait()
-    p.stdout.close()
-
-    return p.returncode
 
 
 def molecular_weight(pdbin):
