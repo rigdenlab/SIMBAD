2db6b753f907b976f2089d88ed9a2fd1d2371703
Felix Simkovic <felixsimkovic@me.com>
2017-07-25 09:43:30 +0100

    order of requirements chagned

--- setup.py
+++ setup.py
@@ -60,10 +60,10 @@ def search_tree(path):
 
 def dependencies():
     return [
+        "pyjob >=0.1.1"
         "numpy >=1.8.2",
         "biopython >=1.64",
         "pandas >=0.17.1",
-        "pyjob >=0.1.1"
     ]
 
 
