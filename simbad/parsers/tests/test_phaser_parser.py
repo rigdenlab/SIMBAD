20c27a629380b5a8bf7ea8cfbffc01b963c47b01
Adam Simpkin <hlasimpk@liv.ac.uk>
2017-08-21 16:46:32 +0530

    Added in unit tests

---
+++ simbad/parsers/tests/test_phaser_parser.py
@@ -0,0 +1,35 @@
+"""Test functions for simbad.parsers.phaser_parser"""
+
+__author__ = "Adam Simpkin"
+__date__ = "17 Aug 2017"
+
+import os
+import tempfile
+import unittest
+
+from simbad.parsers import phaser_parser
+
+class Test(unittest.TestCase):
+    """Unit test"""
+    
+    def test_phaser_parser(self):
+        """Test case for simbad.parsers.phaser_parser.PhaserParser.parse"""
+        
+        # Make tmp file containing key lines from PHASER log
+        phaser_log = tempfile.NamedTemporaryFile("w", delete=False)
+        phaser_log.write("""
+   SOLU SET  RF*0
+   SOLU SET
+   SOLU SET
+   SOLU SET  RF*0 TF*0 LLG=    1419 TFZ==28.9 PAK=0 LLG=1419 TFZ==28.9""")
+        phaser_log.close()
+        
+        PP = phaser_parser.PhaserParser(phaser_log.name)
+        PP.parse()
+        
+        self.assertEqual(PP.llg, 1419)
+        self.assertEqual(PP.tfz, 28.9)
+        self.assertEqual(PP.rfz, None)
+
+if __name__ == "__main__":
+    unittest.main()
