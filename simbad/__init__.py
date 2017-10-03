03f8a5b489b0f8987c55dfc6ba74044eab2b2810
Felix Simkovic <felixsimkovic@me.com>
2017-10-03 17:47:03 +0100

    incorrect path provded

--- simbad/__init__.py
+++ simbad/__init__.py
@@ -17,6 +17,7 @@ if "CCP4" not in os.environ:
     msg = "Cannot find CCP4 root directory"
     raise RuntimeError(msg)
 
-SIMBAD_STATIC_DIR = os.path.join(os.environ["CCP4"], "simbad", "static")
-LATTICE_DB = os.path.join(SIMBAD_STATIC_DIR, "niggli_database.npz")
-CONTAMINANT_MODELS = os.path.join(SIMBAD_STATIC_DIR, 'contaminants')
+SIMBAD_SHARE_STATIC_DIR = os.path.join(os.environ["CCP4"], "share",
+                                       "simbad", "static")
+LATTICE_DB = os.path.join(SIMBAD_SHARE_STATIC_DIR, "niggli_database.npz")
+CONTAMINANT_MODELS = os.path.join(SIMBAD_SHARE_STATIC_DIR, 'contaminants')
