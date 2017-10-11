ac5974174328b0d00766fc3fd52bd1e33a660bf2
Felix Simkovic <felixsimkovic@me.com>
2017-10-11 12:41:39 +0100

    slight refactoring for better compatibility

--- simbad/parsers/rotsearch_parser.py
+++ simbad/parsers/rotsearch_parser.py
@@ -9,10 +9,10 @@ import simbad.parsers
 
 class RotsearchParser(simbad.parsers._Parser):
     """Class to mine information from a rotsearch logfile"""
-    
+
     def __init__(self, logfile):
         super(RotsearchParser, self).__init__(logfile)
-        
+
         self.alpha = None
         self.beta = None
         self.gamma = None
@@ -24,33 +24,34 @@ class RotsearchParser(simbad.parsers._Parser):
         self.cc_f_z_score = None
         self.cc_p_z_score = None
         self.num_of_rot = None
-        
+
         self.parse(logfile)
-        
+
     def parse(self, logfile):
         """Parse information from the logfile"""
-        for line in open(logfile):
-            if line.startswith(" SOLUTIONRCD "):
-                fields = line.split()
-                if float(fields[-3]) > 0:
-                    try:
-                        self.alpha = float(fields[2])
-                        self.beta = float(fields[3])
-                        self.gamma = float(fields[4])
-                        self.cc_f = float(fields[8])
-                        self.rf_f = float(fields[9])
-                        self.cc_i = float(fields[10])
-                        self.cc_p = float(fields[11])
-                        self.icp = float(fields[12])
-                        self.cc_f_z_score = float(fields[-3])
-                        self.cc_p_z_score = float(fields[-2])
-                        self.num_of_rot = float(fields[-1])
+        with open(logfile, "r") as f_in:
+            for line in f_in:
+                fields = line.strip().split()
+                if fields[0] == "SOLUTIONRCD":
+                    if float(fields[-3]) > 0:
+                        try:
+                            self.alpha = float(fields[2])
+                            self.beta = float(fields[3])
+                            self.gamma = float(fields[4])
+                            self.cc_f = float(fields[8])
+                            self.rf_f = float(fields[9])
+                            self.cc_i = float(fields[10])
+                            self.cc_p = float(fields[11])
+                            self.icp = float(fields[12])
+                            self.cc_f_z_score = float(fields[-3])
+                            self.cc_p_z_score = float(fields[-2])
+                            self.num_of_rot = float(fields[-1])
 
-                    except ValueError: 
-                        self.alpha = float(fields[2])
-                        self.beta = float(fields[3])
-                        self.gamma = float(fields[4])
-                        self.cc_f_z_score = float(fields[-3])
-                        self.cc_p_z_score = float(fields[-2])
-                        self.num_of_rot = float(fields[-1])
-                    break
+                        except ValueError:
+                            self.alpha = float(fields[2])
+                            self.beta = float(fields[3])
+                            self.gamma = float(fields[4])
+                            self.cc_f_z_score = float(fields[-3])
+                            self.cc_p_z_score = float(fields[-2])
+                            self.num_of_rot = float(fields[-1])
+                        break
