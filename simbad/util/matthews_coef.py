5e04813c81a83c057fff7666a587e8b87c7ff193
Adam Simpkin <hlasimpk@liv.ac.uk>
2017-10-19 15:05:25 +0200

    Added in a function to suppress the stdout from mmtbx.scaling.matthews.matthews_rupp when a molecule is too large to fit in the ASU

--- simbad/util/matthews_coef.py
+++ simbad/util/matthews_coef.py
@@ -4,10 +4,22 @@ __author__ = "Adam Simpkin"
 __date__ = "10 Oct 2017"
 __version__ = "0.2"
 
+import contextlib
+import io
+import sys
+
 from cctbx.crystal import symmetry
 from mmtbx.scaling.matthews import density_calculator
 from mmtbx.scaling.matthews import matthews_rupp
 from simbad.util.pdb_util import PdbStructure
+
+
+@contextlib.contextmanager
+def no_stdout():
+    save_stdout = sys.stdout
+    sys.stdout = io.BytesIO()
+    yield
+    sys.stdout = save_stdout
 
 
 class SolventContent(object):
@@ -36,5 +48,6 @@ class MatthewsCoefficient(object):
         return self._calculate(struct.nres)
 
     def _calculate(self, nres):
-        result = matthews_rupp(self.crystal_symmetry, n_residues=nres)
-        return result.solvent_content, result.n_copies
+        with no_stdout():
+            result = matthews_rupp(self.crystal_symmetry, n_residues=nres)
+            return result.solvent_content, result.n_copies
