929b18e80803cb94b37b3b07b2db6bca81171043
hlasimpk <hlasimpk@liv.ac.uk>
2018-02-19 18:16:23 +0100

    slight numpy modification

--- simbad/mr/anomalous_util.py
+++ simbad/mr/anomalous_util.py
@@ -135,7 +135,7 @@ class AnomSearch(object):
                     peak = line.split()[5]
                     all_peaks.append(peak)
 
-        z_score = stats.zscore(numpy.array(all_peaks).astype(numpy.float)).max()
+        z_score = stats.zscore(numpy.array(all_peaks, dtype=numpy.float)).max()
 
         score = _AnomScore(dano_peak_height=all_peaks[0],
                            dano_z_score=z_score)
