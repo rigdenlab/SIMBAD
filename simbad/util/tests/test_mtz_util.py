47053cd24d9f839f0334c28b1aeebf48e741abeb
hlasimpk <hlasimpk@liverpool.ac.uk>
2020-06-03 19:56:56 +0100

    Removed crystaldata and getlabels

--- simbad/util/tests/test_mtz_util.py
+++ simbad/util/tests/test_mtz_util.py
@@ -6,6 +6,7 @@ __date__ = "16 Aug 2017"
 import os
 import unittest
 from simbad.command_line import CCP4RootDirectory
+from simbad.parsers import mtz_parser
 from simbad.util import mtz_util
 
 CCP4ROOT = str(CCP4RootDirectory())
@@ -18,29 +19,73 @@ class Test(unittest.TestCase):
         """Test case for mtz_util.crystal_data"""
 
         input_mtz = os.path.join(CCP4ROOT, "examples", "toxd", "toxd.mtz")
-        data = mtz_util.crystal_data(input_mtz)
+        mtz_obj = mtz_parser.MtzParser(input_mtz)
+        data = (
+            "".join(mtz_obj.spacegroup_symbol.encode("ascii").split()),
+            round(mtz_obj.resolution, 2),
+            round(mtz_obj.cell.a, 2),
+            round(mtz_obj.cell.b, 2),
+            round(mtz_obj.cell.c, 2),
+            round(mtz_obj.cell.alpha, 2),
+            round(mtz_obj.cell.beta, 2),
+            round(mtz_obj.cell.gamma, 2)
+        )
 
-        reference_data = ("P212121",
-                          2.300205240684743,
-                          (73.58200073242188, 38.733001708984375, 23.18899917602539, 90.0, 90.0, 90.0))
+        reference_data = (
+            "P212121",
+            2.30,
+            73.58,
+            38.73,
+            23.19,
+            90.00,
+            90.00,
+            90.00
+        )
 
         self.assertEqual(data[0], reference_data[0])
         self.assertAlmostEqual(data[1], reference_data[1])
         self.assertAlmostEqual(data[2], reference_data[2])
+        self.assertAlmostEqual(data[3], reference_data[3])
+        self.assertAlmostEqual(data[4], reference_data[4])
+        self.assertAlmostEqual(data[5], reference_data[5])
+        self.assertAlmostEqual(data[6], reference_data[6])
+        self.assertAlmostEqual(data[7], reference_data[7])
 
     def test_crystal_data_2(self):
         """Test case for mtz_util.crystal_data"""
 
         input_mtz = os.path.join(CCP4ROOT, "examples", "rnase", "rnase25.mtz")
-        data = mtz_util.crystal_data(input_mtz)
+        mtz_obj = mtz_parser.MtzParser(input_mtz)
+        data = (
+            "".join(mtz_obj.spacegroup_symbol.encode("ascii").split()),
+            round(mtz_obj.resolution, 2),
+            round(mtz_obj.cell.a, 2),
+            round(mtz_obj.cell.b, 2),
+            round(mtz_obj.cell.c, 2),
+            round(mtz_obj.cell.alpha, 2),
+            round(mtz_obj.cell.beta, 2),
+            round(mtz_obj.cell.gamma, 2)
+        )
 
-        reference_data = ("P212121",
-                          2.4999665357495098,
-                          (64.89700317382812, 78.322998046875, 38.79199981689453, 90.0, 90.0, 90.0))
+        reference_data = (
+            "P212121",
+            2.50,
+            64.90,
+            78.32,
+            38.79,
+            90.00,
+            90.00,
+            90.00
+        )
 
         self.assertEqual(data[0], reference_data[0])
         self.assertAlmostEqual(data[1], reference_data[1])
         self.assertAlmostEqual(data[2], reference_data[2])
+        self.assertAlmostEqual(data[3], reference_data[3])
+        self.assertAlmostEqual(data[4], reference_data[4])
+        self.assertAlmostEqual(data[5], reference_data[5])
+        self.assertAlmostEqual(data[6], reference_data[6])
+        self.assertAlmostEqual(data[7], reference_data[7])
 
     def test_get_labels_1(self):
         """Test case for mtz_util.get_labels"""
@@ -49,11 +94,12 @@ class Test(unittest.TestCase):
         temp_mtz = os.path.join(os.getcwd(), "input.mtz")
         temp_log = os.path.join(os.getcwd(), "input.log")
         mtz_util.ctruncate(input_mtz, temp_mtz)
-        mtz_labels = mtz_util.GetLabels(temp_mtz)
+        mtz_obj = mtz_parser.MtzParser(temp_mtz)
+        mtz_obj.parse()
         os.remove(temp_mtz)
         os.remove(temp_log)
 
-        data = (mtz_labels.f, mtz_labels.sigf, mtz_labels.free)
+        data = (mtz_obj.f, mtz_obj.sigf, mtz_obj.free)
 
         reference_data = ("FTOXD3", "SIGFTOXD3", "FreeR_flag")
 
@@ -66,12 +112,13 @@ class Test(unittest.TestCase):
         temp_mtz = os.path.join(os.getcwd(), "input.mtz")
         temp_log = os.path.join(os.getcwd(), "input.log")
         mtz_util.ctruncate(input_mtz, temp_mtz)
-        mtz_labels = mtz_util.GetLabels(temp_mtz)
+        mtz_obj = mtz_parser.MtzParser(temp_mtz)
+        mtz_obj.parse()
         os.remove(temp_mtz)
         os.remove(temp_log)
 
-        data = (mtz_labels.f, mtz_labels.sigf, mtz_labels.fplus, mtz_labels.sigfplus, mtz_labels.fminus,
-                mtz_labels.sigfminus, mtz_labels.free)
+        data = (mtz_obj.f, mtz_obj.sigf, mtz_obj.f_plus, mtz_obj.sigf_plus, mtz_obj.f_minus,
+                mtz_obj.sigf_minus, mtz_obj.free)
         reference_data = ("FNAT", "SIGFNAT", "FPTNCD25(+)", "SIGFPTNCD25(+)",
                           "FPTNCD25(-)", "SIGFPTNCD25(-)", "FreeR_flag")
 
@@ -82,14 +129,36 @@ class Test(unittest.TestCase):
         input_mtz = os.path.join(CCP4ROOT, "examples", "toxd", "toxd.mtz")
         temp_mtz = os.path.join(os.getcwd(), "input.mtz")
         mtz_util.reindex(input_mtz, temp_mtz, "18")
+        mtz_obj = mtz_parser.MtzParser(temp_mtz)
+        data = (
+            "".join(mtz_obj.spacegroup_symbol.encode("ascii").split()),
+            round(mtz_obj.resolution, 2),
+            round(mtz_obj.cell.a, 2),
+            round(mtz_obj.cell.b, 2),
+            round(mtz_obj.cell.c, 2),
+            round(mtz_obj.cell.alpha, 2),
+            round(mtz_obj.cell.beta, 2),
+            round(mtz_obj.cell.gamma, 2)
+        )
+        reference_data = (
+            "P21212",
+            2.30,
+            73.58,
+            38.73,
+            23.19,
+            90.00,
+            90.00,
+            90.00
+        )
 
-        data = mtz_util.crystal_data(temp_mtz)
-        reference_data = ("P21212",
-                          2.300205240684743,
-                          (73.58200073242188, 38.733001708984375, 23.18899917602539, 90.0, 90.0, 90.0))
         self.assertEqual(data[0], reference_data[0])
         self.assertAlmostEqual(data[1], reference_data[1])
         self.assertAlmostEqual(data[2], reference_data[2])
+        self.assertAlmostEqual(data[3], reference_data[3])
+        self.assertAlmostEqual(data[4], reference_data[4])
+        self.assertAlmostEqual(data[5], reference_data[5])
+        self.assertAlmostEqual(data[6], reference_data[6])
+        self.assertAlmostEqual(data[7], reference_data[7])
 
 
 if __name__ == "__main__":
